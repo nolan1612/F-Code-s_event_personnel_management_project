00097e38639799379ebe890fb8b10eb0273eefc5
nolan1612 <minhnhat071612@gmail.com>
2026-04-22 12:27:26 +0700

    add main function

--- src/auth.c
+++ src/auth.c
@@ -1,1 +1,3 @@
 // Code các hàm xử lý đăng nhập, đếm số lần nhập sai
+#include "auth.h"
+#include <stdio.h>
--- src/event.c
+++ src/event.c
@@ -1,1 +1,3 @@
 // Code các hàm Thêm / Sửa / Xóa / Chuyển trạng thái sự kiện
+#include "event.h"
+#include <stdio.h>
