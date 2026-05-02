edf2f79655bfa7148a0096b552bef86e0d47d8b7
Este4 <ledangminh1206@gmail.com>
2026-05-02 18:57:04 +0700

    refactor: update input and output for file

--- includes/fileio.h
+++ includes/fileio.h
@@ -4,10 +4,8 @@
 #include "event.h"
 #include "auth.h"
 #include <stdio.h>
-
 void saveAccounts(Account list[], int count);
 int loadAccounts(Account list[]);
-
 void saveEvents(Event list[], int count);
 int loadEvents(Event list[]);
 
--- src/fileio.c
+++ src/fileio.c
@@ -5,19 +5,10 @@
 #include "../includes/report.h"
 #include "../includes/staff.h"
 #include "../includes/utils.h"
-
-
-
-// void saveAccounts(Account list[], int count) {
-
-//     FILE *file = fopen("account.dat", "wb"); 
-//     if (file == NULL) {
-//         printf("Loi: Khong the ghi file accounts.dat!\n");
-//         return;
-//     }
-//     fwrite(list, sizeof(Account), count, file);
-//     fclose(file);
-// }
+void saveAccounts(Account list[], int count);
+void saveAccounts(Account list[], int count);
+void saveEvents(Event list[], int count);
+int loadEvents(Event list[]);
 void saveAccounts(Account list[], int count) {
     FILE *file = fopen("data/account.dat", "w"); 
     
@@ -39,35 +30,6 @@ void saveAccounts(Account list[], int count) {
     
     fclose(file);
 }
-
-// int loadAccounts(Account list[]) {
-//     FILE *file = fopen("account.dat", "rb"); 
-//     if (file == NULL) {
-//         printf("[He thong] File accounts.dat chua ton tai. Dang tao du lieu mau...\n");
-        
-//         Account mockData[5] = {
-//             {"SE211377", "Ho26072007*", 1, 0, 0}, 
-//             {"SE210023", "1234567789", 0, 0, 0},  
-//             {"SE211577", "234567890", 0, 0, 0},
-//             {"SE210182", "345678901", 0, 0, 0},
-//             {"SE205432", "ui1234567", 1, 0, 0}  
-//         };
-        
-//         for (int i = 0; i < 5; i++) {
-//             list[i] = mockData[i];
-//         }
-        
-//         saveAccounts(list, 5);
-//         return 5; 
-//     }
-    
-//     int count = 0;
-//     while (fread(&list[count], sizeof(Account), 1, file) == 1) {
-//         count++;
-//     }
-//     fclose(file);
-//     return count;
-// }
 
 
 int loadAccounts(Account list[]) {
